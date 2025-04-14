c64184b51e4bcd14d9499ba3d20e631a4c2e54d0
lodrol132 <maksimvilkov132@gmail.com>
2025-04-14 20:31:56 +1000

    0.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,21 @@ document.addEventListener("mousemove", (event) => {
     Object.assign(document.documentElement, styles)
 })
 
+let xDeg = document.querySelector(".x-deg")
+let yDeg = document.querySelector(".y-deg")
+
+
+window.addEventListener("deviceorientation", (event)=>{
+    let xRotate = event.beta
+    let yRotate = event.gamma
+
+    xDeg.innerHTML = xRotate
+    yDeg.innerHTML = yRotate
+
+    const styles = {
+        style: `--move-x: ${((event.beta - window.innerWidth) / 2) * 0.005}deg;
+                --move-y: ${((event.gamma - window.innerHeight) / 2) * 0.01}deg;`
+      };
+      
+      Object.assign(document.documentElement, styles);
+}, true)
