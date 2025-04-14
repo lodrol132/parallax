4b9e4a2ee53be738c6392803fb1f74c1345a53f2
lodrol132 <maksimvilkov132@gmail.com>
2025-04-14 20:40:49 +1000

    0.1.5

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,8 +18,8 @@ window.addEventListener("deviceorientation", (event)=>{
     yDeg.innerHTML = yRotate
 
     const styles = {
-        style: `--move-x: ${xRotate}deg;
-                --move-y: ${yRotate}deg;`
+        style: `--move-x: ${xRotate / 2}deg;
+                --move-y: ${yRotate / 2}deg;`
       }
       
     Object.assign(document.documentElement, styles);
