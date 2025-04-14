0284219ad37d208fef176c210a0642e8729893a6
lodrol132 <maksimvilkov132@gmail.com>
2025-04-14 20:52:54 +1000

    0.2.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("mousemove", (event) => {
     const styles = {
-        style: `--move-x: ${(event.clientX - window.innerWidth / 2)}deg;
-        --move-y: ${(event.clientY - window.innerHeight / 2)}deg;`
+        style: `--move-x: ${(event.clientX - window.innerWidth / 2)*0.005}deg;
+        --move-y: ${(event.clientY - window.innerHeight / 2 )*0.01}deg;`
     }
     Object.assign(document.documentElement, styles)
 })
@@ -9,18 +9,18 @@ document.addEventListener("mousemove", (event) => {
 let xDeg = document.querySelector(".x-deg")
 let yDeg = document.querySelector(".y-deg")
 
-
-window.addEventListener("deviceorientation", (event)=>{
-    let xRotate = event.alpha
-    let yRotate = event.gamma
+window.addEventListener("deviceorientation", (event) => {
+    let xRotate = event.gamma
+    let yRotate = event.alpha
+    console.log(xRotate, yRotate);
 
     xDeg.innerHTML = xRotate
     yDeg.innerHTML = yRotate
 
     const styles = {
         style: `--move-x: ${xRotate / 2}deg;
-                --move-y: ${yRotate / 2}deg;`
-      }
-      
-    Object.assign(document.documentElement, styles);
+        --move-y: ${yRotate / 2}deg;`
+    }
+    Object.assign(document.documentElement, styles)
+
 }, true)
